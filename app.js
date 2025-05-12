d19f6adbbc98337a8267081f4a5262fc02c18a8b
Karun Kumar <karunkumar@Karuns-MacBook-Air.local>
2025-05-12 18:13:23 +0530

    updated

--- app.js
+++ app.js
@@ -87,6 +87,10 @@ app.post("/submitForm", async (req, res) => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
